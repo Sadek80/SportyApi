3b9af702b43d10b2e3fcff46144a9426d011b047
Sadek80 <mohamedsadk80@gmail.com>
2022-02-27 09:06:22 +0200

    AddsProgramMappingProfile

---
+++ Profiles/TrainingProgramProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportyApi.Profiles
+{
+    public class TrainingProgramProfile : Profile
+    {
+        public TrainingProgramProfile(IServiceProvider service)
+        {
+
+        }
+    }
+}
